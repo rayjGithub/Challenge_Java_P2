5984bb99057366189dba39ea10909e3ab8a58b68
Regis Eboa <regiseboa@gmail.com>
2022-05-03 11:52:20 +0200

    Modification de la classe Mouse: ajout héritage, appel constructeur parent, spécialisation de la methode toString()

--- src/com/openclassrooms/store/Mouse.java
+++ src/com/openclassrooms/store/Mouse.java
@@ -1,13 +1,14 @@
 package com.openclassrooms.store;
 
-public class Mouse {
+public class Mouse extends Item{
 
-    public String brand;
-    public double price;
+    public Mouse(Brand brand, double price) {
+        super(brand, price);// call from the Item class' constructor
+    }
 
-    public Mouse(String a, double b) {
-        this.brand = a; // Updating brand field
-        this.price = b; // Updating price field
+    // Spécialisation de la methode toString pour la classe Mouse
+    public String toString() {
+        return "Mouse [brand=" + brand + ", price=" + price + "]";
     }
 
 }
