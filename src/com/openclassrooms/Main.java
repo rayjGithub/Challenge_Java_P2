dd1defc939e3319605b876e996c2f37d40b847c2
Regis Eboa <regiseboa@gmail.com>
2022-05-03 12:03:24 +0200

    Test: instanciation de nouveaux objets Mouse et Screen, puis affichage avec la methode toString sur la console

--- src/com/openclassrooms/Main.java
+++ src/com/openclassrooms/Main.java
@@ -1,33 +1,41 @@
 package com.openclassrooms;
 
-import com.openclassrooms.store.Inventory;
+import com.openclassrooms.store.Brand;
 import com.openclassrooms.store.Mouse;
 import com.openclassrooms.store.Screen;
+
+// import com.openclassrooms.store.Inventory;
+// import com.openclassrooms.store.Mouse;
+// import com.openclassrooms.store.Screen;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Inventory POC");
+        // System.out.println("Inventory POC");
 
-        Mouse m = new Mouse("DELL", 20.0);
-        Screen s = new Screen("Samsung", 150.0, "1920x1080");
+        // Mouse m = new Mouse("DELL", 20.0);
+        // Screen s = new Screen("Samsung", 150.0, "1920x1080");
 
-        Inventory inventory = new Inventory();
-        inventory.addScreen(s);
-        inventory.addMouse(m);
+        // Inventory inventory = new Inventory();
+        // inventory.addScreen(s);
+        // inventory.addMouse(m);
 
-        for(int i=0; i < inventory.screens.length; i++ ) {
-            if(inventory.screens[i] != null) {
-                System.out.println(inventory.screens[i].brand);
-            }
-        }
+        // for(int i=0; i < inventory.screens.length; i++ ) {
+        //     if(inventory.screens[i] != null) {
+        //         System.out.println(inventory.screens[i].brand);
+        //     }
+        // }
 
-        for(int j = 0; j < inventory.mice.length; j++ ) {
-            if(inventory.mice[j] != null) {
-                System.out.println(inventory.mice[j].brand);
-            }
-        }
+        // for(int j = 0; j < inventory.mice.length; j++ ) {
+        //     if(inventory.mice[j] != null) {
+        //         System.out.println(inventory.mice[j].brand);
+        //     }
+        // }
+        Mouse delMouse = new Mouse(Brand.DELL, 20.0);
+        Screen samsungScreen = new Screen(Brand.SAMSUNG, 150.0, "1920x10800");
 
+        System.out.println(delMouse.toString());
+        System.out.println(samsungScreen.toString());
 
     }
 }
